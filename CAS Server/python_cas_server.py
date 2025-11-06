8ffc5e51deb1fd0b4b0f4a2f2e283dbbc60f7f3a
dolcos <alfonso.then@gmail.com>
2025-11-06 08:56:25 -0400

    cas close conn
    Removed unnecessary comments and whitespace, and added connection close.

--- CAS Server/python_cas_server.py
+++ CAS Server/python_cas_server.py
@@ -1,44 +1,20 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[18]:
-
 
 import swat
 import os
-
-
-# In[19]:
-
 
 cashost = 'sas-cas-server-default-client'
 casport = 5570
 passwordToken = os.environ.get('ACCESS_TOKEN')
 
-
-# In[21]:
-
-
 conn = swat.CAS(hostname=cashost, port=casport, password=passwordToken)
-
-
-# In[24]:
-
 
 display(conn)
 print(conn.serverstatus())
 type(conn)
 conn.about()['About']['Viya Version']
 
-
-# In[23]:
-
-
 conn.fileInfo(caslib = 'casuser')
-
-
-# In[28]:
-
 
 import pandas as pd
 import numpy as np
@@ -46,10 +22,6 @@ import numpy as np
 conn.read_csv('data/courses/CASL/data/my_data/cars.csv', casout={'name':'cars', 'caslib':'casuser', 'replace':True})
 
 conn.fileInfo(caslib = 'casuser')
-
-
-# In[30]:
-
 
 castbl = conn.CASTable('cars', caslib = 'casuser')
 
@@ -61,9 +33,6 @@ df = castbl.head()
 
 display(df)
 
-
-# In[ ]:
-
-
+conn.close()
 
 
